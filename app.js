35d4fbe33a146825fd7d60b4f57445f682e991ec
Joel Sabillon <jsabillon@beachbody.com>
2015-02-05 18:16:33 -0800

    Changing port of server

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res){
 });
 
 
-var server = app.listen(3000, function(){
-	console.log("Listening on port 3000");
+var server = app.listen(80, function(){
+	console.log("Listening on port 80");
 });
