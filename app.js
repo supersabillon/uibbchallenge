8b31a5e51373f5ca4e3ac24ef916eaeb0bdb92a5
Joel Sabillon <jsabillon@beachbody.com>
2015-02-06 11:17:42 -0800

    Adding AWS Sdk

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var AWS = require('aws-sdk');
 var app = express();
 
 app.get('/', function(req, res){
@@ -6,6 +7,6 @@ app.get('/', function(req, res){
 });
 
 
-var server = app.listen(80, function(){
+var server = app.listen(3000, function(){
 	console.log("Listening on port 80");
 });
